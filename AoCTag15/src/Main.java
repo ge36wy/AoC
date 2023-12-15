4487f244ba879081581dabd720bf0e00744ff4d1
ge36wy <sejoklee@web.de>
2023-12-15 15:30:57 +0100

    AoC Tag 15 Teil 2

--- AoCTag15/src/Main.java
+++ AoCTag15/src/Main.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,13 +11,38 @@ public class Main {
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
             String line;
             long total = 0;
-            ArrayList<char[]> layout = new ArrayList<>();
+            HashMap<Integer,ArrayList<String>> map = new HashMap<>();
+            for(int i = 0; i < 256; i++){
+                map.put(i, new ArrayList<>());
+            }
+            ArrayList<String> layout = new ArrayList<>();
             while ((line = reader.readLine()) != null) {
                 String[] codes = line.split(",");
-                for(String s: codes) layout.add(s.toCharArray());
+                layout.addAll(Arrays.asList(codes));
             }
-            for(char[] l: layout){
-                total += calcHash(l);
+            for(String l: layout){
+                int last = Math.max(l.indexOf('='), l.indexOf('-'));
+                String label = l.substring(0, last);
+                int b = calcHash(label.toCharArray());
+                if(l.charAt(last) == '-'){
+                    map.get(b).removeIf(s -> s.startsWith(label + " "));
+                }else{
+                    if(map.get(b).stream().anyMatch(p -> p.startsWith(label + " "))) {
+                        ArrayList<String> box = map.get(b);
+                        int i;
+                        for (i = 0; i < box.size(); i++) {
+                            if(box.get(i).startsWith(label + " ")) break;
+                        }
+                        map.get(b).set(i, label + " " + l.charAt(last + 1));
+                    }else{
+                        map.get(b).add(label + " " + l.charAt(last + 1));
+                    }
+                }
+            }
+            for(int i = 0; i < 256; i++){
+                for(int j = 0; j < map.get(i).size(); j++){
+                    total += (long) (i + 1) * (j + 1) * Integer.parseInt(map.get(i).get(j).split("\s")[1]);
+                }
             }
             System.out.println(total);
         } catch (Exception e) {
